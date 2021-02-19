cf562db375b20b6b3519390499d9405ba4b9a264
Samuel <sammorris207@gmail.com>
2021-02-19 21:47:07 +0530

    commit-6

---
+++ usinddir/admin/include/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "u165068274_usinddir", "Password@9848", "u165068274_usinddir");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ usinddir/admin/include/footer.php
@@ -0,0 +1,7 @@
+ <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © <a href="http://hindustansoft.in/" target="_blank">Hindustan Enterprises 2020</a></span>
+          </div>
+        </div>
+      </footer>
